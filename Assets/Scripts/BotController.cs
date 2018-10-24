d047771bf086253c66903c54837a028867530ba1
Eric Hao <hao.awesome@gmail.com>
2018-10-23 18:19:07 -0700

    initial commit

---
+++ Assets/Scripts/BotController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BotController : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody2D body;
+
+    private void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        body.AddForce(movement * speed);
+    }
+}
